08772b5278c67a564ba80d020d3384e5e8891312
Ryz Jenvey <rjenvey@hotmail.co.uk>
2021-01-05 17:26:55 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Helper scripts
+
+A simpler way to stop me forgetting how to add scripts to my path.
+
+`./setup.sh` run once to add this project to PATH and export SCRIPTS_PATH variable
+
+`lss` displays all helper scripts and symlinks
+
+`lns <filename>` add a symlink to PATH
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+CONFIG_FILE="$HOME/".zshrc
+
+if test -f "$CONFIG_FILE"; then
+  # shellcheck disable=SC2129
+  echo >>"$CONFIG_FILE"
+  echo \# Added by helper-scripts project >>"$CONFIG_FILE"
+  echo export SCRIPTS_PATH="$(pwd)" >>"$CONFIG_FILE"
+  echo export PATH="$(pwd)"/symlinks:"$(pwd)"/scripts:\$PATH >>"$CONFIG_FILE"
+else
+  echo no .zshrc file! maybe you need to create one or you\'re using bash!
+fi
