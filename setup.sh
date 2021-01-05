7544f8b8b436c6f5c2533336aae790901c8b1e27
Ryz Jenvey <rjenvey@hotmail.co.uk>
2021-01-05 17:28:44 +0000

    small wording update to setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ if test -f "$CONFIG_FILE"; then
   echo export SCRIPTS_PATH="$(pwd)" >>"$CONFIG_FILE"
   echo export PATH="$(pwd)"/symlinks:"$(pwd)"/scripts:\$PATH >>"$CONFIG_FILE"
 else
-  echo no .zshrc file! maybe you need to create one or you\'re using bash!
+  echo no .zshrc file! maybe you need to create one or maybe you\'re using bash!
 fi
